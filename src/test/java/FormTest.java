936de6dffcc471640ff4df3207212eee4d01f9fe
Sasha Zin <zinalbo@tut.by>
2023-05-26 20:26:31 +0300

    HW_03

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,5 +1,4 @@
-import org.testng.Assert;
-import static org.testng.Assert.assertTrue;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,32 +8,35 @@ import org.testng.annotations.Test;
 public class FormTest {
     @Test
     public void checkRegistrationFormData() {
-        WebDriver driver;
+        WebDriver driver = new ChromeDriver();
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://demoqa.com/automation-practice-form");
 
-        WebElement firstNameField = driver.findElement(By.id("firstName"));
+        WebElement firstNameField = driver.findElement(By.xpath("//input[@id=\"firstName\"]"));
         firstNameField.sendKeys("Sasha");
 
-        WebElement lastNameField = driver.findElement(By.xpath("//input[@placeholder=\"Last Name\"]"));
+        WebElement lastNameField = driver.findElement(By.xpath("//input[@placeholder='Last Name']"));
         lastNameField.sendKeys("Zinchuk");
 
-        WebElement maleRadioButton = driver.findElement(By.xpath("//label[@for=\"gender-radio-1\"]"));
-        maleRadioButton.click();
+        WebElement eMailField = driver.findElement(By.xpath("//input[@class='mr-sm-2 form-control']"));
+        // Этот сласс - единственный. Выделяет только его. Есть еще 3 таких же, но с пробелом в начале. Зачем так сделано? Косяк разработчика?
+        eMailField.sendKeys("abrakadabra@site.com");
 
-        WebElement mobileNumberField = driver.findElement(By.xpath("//input[@placeholder='Mobile Number']"));
-        mobileNumberField.sendKeys("1234567890");
+        WebElement genderFemaleRadioButton = driver.findElement(By.xpath("//label[@for=\"gender-radio-1\"]"));
+        genderFemaleRadioButton.click();
 
-//        WebElement submitButton = driver.findElement(By.xpath("//button[@type='submit']"));
-//        submitButton.click();
+        WebElement dateOfBirthField = driver.findElement(By.xpath("//*[@id=\"dateOfBirthInput\"]"));
+        dateOfBirthField.click();                // Открываем календарь для выбора даты
+        WebElement dateOfBirthChoose = driver.findElement(By.xpath("//div[@aria-label='Choose Monday, May 1st, 2023']"));
+        dateOfBirthChoose.click();
 
-//        WebElement userDataText = driver.findElement(By.xpath("//tr[1]/td[1]/following-sibling::td"));
+        // Закомментил, потому как не происходит активация чек-бокса. Это делается не через click() ?
+//        WebElement hobbyCheckBoxes = driver.findElement(By.xpath("//input[@type='checkbox'][@value='1']"));
+//        hobbyCheckBoxes.click();
 
-//        Assert.assertTrue(userDataText.getText().contains("Sasha Zinchuk"));
-
-//        driver.close();
-//        driver.quit();
+        // Закомментил, потому как не нажимается кнопка. Это делается не через click() ?
+//        WebElement selectPictureButton = driver.findElement(By.xpath("//input[@type='file']"));
+//        selectPictureButton.click();
     }
 }
